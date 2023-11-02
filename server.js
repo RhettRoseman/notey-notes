46fbf8a39f224ec0ca4cef19fea27f625fbbf4e2
RhettRoseman <rroseman@rcplanners.com>
2023-11-02 15:23:40 -0400

    updating PORT AGAIN

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 
 const path = require('path');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Initialize our app variable by setting it to the value of express()
 const app = express();
