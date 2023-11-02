23a2096969302a387a333ad3466e8e149e346523
RhettRoseman <rroseman@rcplanners.com>
2023-11-02 15:18:37 -0400

    commented line 63 in serverjs

--- server.js
+++ server.js
@@ -53,14 +53,14 @@ app.post('/api/notes', (req, res) =>  {
 
 app.get('/index', (req, res) => res.json(termData));
 
-app.get('/', (req, res) => res.send('Visit http://localhost:3001/api'));
+app.get('/', (req, res) => res.send(`(Visit http://localhost:${PORT}/api`));
 
 // res.json() allows us to return JSON instead of a buffer, string, or static file
 app.get('/api', (req, res) => res.json(termData));
 
 // require('/db/db.json')(app);
 
-require('/api/notes.html')(app);
+// require('/api/notes.html')(app);
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
